7bdd9a13895bab38a9c73cce8a15c9e0fd487893
saunic <saunic8@gmail.com>
2022-03-25 10:07:49 -0600

    Bot responds to commands !hello !ping and !embed

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 // When the client is ready, run this code (only once)
-client.once("ready", () => {
+client.on("ready", () => {
   console.log("Ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith("!hello")) {
+    message.channel.send("hello!");
+  } else if (message.content.startsWith("!ping")) {
+    message.channel.send("Pong!");
+  } else if (message.content.startsWith("!embed")) {
+    const embed = new MessageEmbed()
+      .setTitle("Embed")
+      .setDescription(message.content)
+      .setAuthor({
+        name: message.author.username,
+        iconURL: "https://i.imgflip.com/69vvn3.jpg",
+        url: message.url,
+      });
+
+    message.channel.send({
+      content: "Hey this is your embed",
+      embeds: [embed],
+    });
+  }
 });
 
 // Login to Discord with your client's token
