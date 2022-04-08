1fab084bb9c19c865cd151d047dbd27d8e9bd823
saunic <saunic8@gmail.com>
2022-04-08 09:59:55 -0600

    create "add" slash command

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 // Require the necessary discord.js classes
 const { Client, Intents, MessageEmbed } = require("discord.js");
-const { token } = require("./config.json");
+const { token, clientID, guildID } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
 // When the client is ready, run this code (only once)
 client.on("ready", () => {
   console.log("Ready!");
+});
+
+// Slash commands
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("add")
+    .setDescription("adds two numbers together")
+    .addIntegerOption((option) =>
+      option.setName("num1").setDescription("first number").setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option.setName("num2").setDescription("second number").setRequired(true)
+    ),
+].map((command) => command.toJSON());
+const rest = new REST({ version: 9 }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientID, guildID), {
+  body: commands,
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.getInteger("num1");
+    const num2 = interaction.options.getInteger("num2");
+    await interaction.reply(`${num1 + num2}`);
+    return;
+  }
+  // if (interaction.commandName === "hello") {
+  //   await interaction.reply("Hello World!");
+  //   return;
+  // }
+  // if (interaction.commandName === "info") {
+  //   await infoHandler(interaction);
+  //   return;
+  // }
 });
 
 client.on("messageCreate", (message) => {
